e6ef3a0acbd87d4dc596976c00dc82504d8f531a
anass ibnou ali <a.ibnouali@insea.ac.ma>
2018-10-09 11:42:31 +0100

    add frontend part without css

---
+++ users/src/app/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { User } from './model';
+
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class UserService {
+
+    constructor(private http: HttpClient) { }
+
+    // private userUrl = 'http://localhost:8080/user-portal/user';
+    private userUrl = 'http://localhost:8088/api/users';
+
+    public getUsers() {
+        return this.http.get<User[]>(this.userUrl);
+    }
+
+    public deleteUser(user) {
+        return this.http.delete(this.userUrl + '/' + user.id);
+    }
+
+    public createUser(user) {
+        return this.http.post<User>(this.userUrl, user);
+    }
+
+}
