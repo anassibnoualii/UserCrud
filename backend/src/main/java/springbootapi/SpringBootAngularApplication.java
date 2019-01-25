945fd6091fbcf65df730c2272f63b9ee55d23d09
anassibnoualii <34884044+anassibnoualii@users.noreply.github.com>
2019-01-25 17:49:57 +0100

    Update SpringBootAngularApplication.java

--- backend/src/main/java/springbootapi/SpringBootAngularApplication.java
+++ backend/src/main/java/springbootapi/SpringBootAngularApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class SpringBootAngularApplication {
 
 	public static void main(String[] args) {
