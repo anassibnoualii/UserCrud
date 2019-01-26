b9a7cc7dbb57f125d4db33f9999eccdba959ffef
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-01-26 01:05:21 +0100

    [feature] - adding api description

--- backend/src/main/java/springbootapi/controller/UserController.java
+++ backend/src/main/java/springbootapi/controller/UserController.java
@@ -3,6 +3,10 @@ package springbootapi.controller;
 import java.util.List;
 import java.util.Optional;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +26,8 @@ import springbootapi.service.UserService;
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
+@Api(value = "users", tags = "manage usesr")
+
 public class UserController {
 	@Autowired
 	UserRepository userRepository;
@@ -29,28 +35,39 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	@GetMapping("/users")
+    @ApiOperation(value = "retrieve all users from data base")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")})
+    @GetMapping("/users")
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
 
-	@GetMapping(path = { "/users/{id}" })
+    @ApiOperation(value = "retrieve user by id  from data base")
+
+    @GetMapping(path = { "/users/{id}" })
 	public UserDTO findOne(@PathVariable("id") Long id) {
 		return userService.findUser(id);
 	}
 
-	@PostMapping("/users")
+    @ApiOperation(value = "add user user to data base")
+    @PostMapping("/users")
 	public User create(@RequestBody User user) {
 		return userRepository.save(user);
 
 	}
 
+    @ApiOperation(value = "updating a user userin a data base")
 	@PutMapping("/users")
 	public User update(@RequestBody User user) {
 		return userRepository.saveAndFlush(user);
 	}
 
-	@DeleteMapping(path = { "/users/{id}" })
+    @ApiOperation(value = "delete a user from data base")
+    @DeleteMapping(path = { "/users/{id}" })
 	public void delete(@PathVariable("id") Long id) {
 		
 		userRepository.deleteById(id);
